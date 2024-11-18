27fea3920345e08e5b8d54dd60e853187657553b
spifory <spifory@protonmail.com>
2024-11-18 13:10:32 +0200

    refactor(scripts): use notify-send instead of dunstify

--- hypr/scripts/brightness.sh
+++ hypr/scripts/brightness.sh
@@ -6,10 +6,10 @@ case $1 in
         if [ $new_brightness == $current_brightness ]; then
             exit 1
         fi
-        dunstify --replace=3 -h int:value:$new_brightness --urgency=normal --icon="display-brightness" "BRIGHTNESS" "Brightness increased to $new_brightness"
+        notify-send --replace-id=3 -h int:value:$new_brightness --urgency=normal --icon="display-brightness" "BRIGHTNESS" "Brightness increased to $new_brightness"
         ;;
     down)
         new_brightness=$(brightnessctl set -m 5%- | cut -d, -f4)
-        dunstify --replace=3 -h int:value:$new_brightness --urgency=normal --icon="display-brightness" "BRIGHTNESS" "Brightness decreased to $new_brightness" 
+        notify-send --replace-id=3 -h int:value:$new_brightness --urgency=normal --icon="display-brightness" "BRIGHTNESS" "Brightness decreased to $new_brightness" 
         ;;
 esac
